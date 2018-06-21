5f7a9eedbe43e86f8b9679e1d45a3c3a2c58f8e0
wp01053 <wp01053@naver.com>
2018-06-21 09:49:27 +0900

    자바 수업

---
+++ java/ch02/src/ch02/UseVariable.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class UseVariable {
+
+	public static void main(String[] args) {
+		double num1, num2;
+		double result;
+		num1 = 1.01;
+		num2 = 2.01;
+		result = num1 + num2;
+		System.out.println(result);
+
+	}
+
+}
